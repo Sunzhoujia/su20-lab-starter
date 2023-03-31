3141404dd4a0e6d87e8d759fde2154e1c2cfbb9f
sunzhoujia <sunzhoujia1030@163.com>
2023-03-31 09:03:33 +0800

    lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,24 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    node* slow = head;
+    node* fast = head;
+
+    if (!head || !head->next) {
+      return 0;
+    }
+
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        break;
+      }
+    }
+    if (!fast || !fast->next) {
+      return 0;
+    }
+
+
+    return 1;
 }
