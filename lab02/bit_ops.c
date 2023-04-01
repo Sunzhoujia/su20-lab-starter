eddf2788c97bda5fd66e01e884c5f45328fefa97
sunzhoujia <sunzhoujia1030@163.com>
2023-04-01 14:25:46 +0800

    lab02

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,6 +9,11 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns
     // 0 or 1)
+    if ((x >> n) & 1) {
+      return 1;
+    } else {
+      return 0;
+    }
     return -1;
 }
 // Set the nth bit of the value of x to v.
@@ -17,11 +22,17 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    if (v) {
+      *x = *x | (1 << n);
+    } else {
+      *x = *x & ~(1 << n);
+    }
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    *x = *x ^ (1 << n);
 }
 
